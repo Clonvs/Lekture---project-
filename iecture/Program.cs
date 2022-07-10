292730b167f051c01a7711581213bb68af8e71a1
Igor <igor.borodin.888@gmail.com>
2022-07-10 05:29:08 +0300

    Hello user

--- iecture/Program.cs
+++ iecture/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World2!");
+﻿//Console.WriteLine("Hello, World2!");
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет");
+Console.Write(username);
